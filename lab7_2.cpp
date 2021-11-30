45955845bf3651f0298b9e6feaaf2aa09ce4e39d
Setthanuwat <aftnick2@gmail.com>
2021-11-30 15:50:19 +0700

    "lap7_2"

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,40 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	int i = 0, k,P[5000], result;
+	while(i < 5000)
+    {
 		P[i] = 2*i+1;
 		i++;
 	}
+
+	cout << "Please input k: " ;
+	cin >> k ;
 	
+    if(k > 0)
+	{
+        result = 0;
+		i = 0;
+	}
+	else
+	{
+       cout << "Invalid input!!!" ;
+       return 0;
+	}
+	while(i < 5000){
+		if(P[i]%k == 0)
+		{
+			result = result - P[i] ;
+			i++;
+		}
+		else
+		{
+			result = result + P[i];
+		     i++;
+		}
+		
+	}
+	cout << result ;
+
 	return 0;
 }
